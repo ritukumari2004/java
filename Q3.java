769cf19a1006e925a7154353f9bce307bc23c847
ritukumari2004 <127641811+ritukumari2004@users.noreply.github.com>
2023-03-16 23:21:24 +0530

    Add files via upload
    Q 3

---
+++ Q3.java
@@ -0,0 +1,16 @@
+
+public class Q3 {
+
+	public static void main(String[] args) {
+		System.out.println("RRRRRR      K       K  ");
+		System.out.println("R    R      K     K    ");
+		System.out.println("R   R       K  K       ");
+		System.out.println("R R         K          ");
+		System.out.println("R  R        K  K       ");
+		System.out.println("R    R      K     K    ");
+		System.out.println("R      R    K       K  ");
+		
+
+	}
+
+}
