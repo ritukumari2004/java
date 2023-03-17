880eae4d1ed205091c3212680220c32b1895dae7
ritukumari2004 <127641811+ritukumari2004@users.noreply.github.com>
2023-03-17 23:22:57 +0530

    Add files via upload
    Q4

---
+++ Q4.java
@@ -0,0 +1,15 @@
+
+
+public class Q4 {
+
+	public static void main(String[] args) {
+		System.out.println("     *        ");
+		System.out.println("    * *       ");
+		System.out.println("   * * *      ");
+		System.out.println("  * * * *     ");
+		System.out.println(" * * * * *    ");
+		
+
+	}
+
+}
