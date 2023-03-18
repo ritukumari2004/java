52d1513c7c51971b3f78e339c11475d1a9ac1d1c
ritukumari2004 <127641811+ritukumari2004@users.noreply.github.com>
2023-03-18 22:14:52 +0530

    Add files via upload
    Q5

---
+++ Q5.java
@@ -0,0 +1,12 @@
+
+
+public class Q5 {
+
+	public static void main(String[] args) {
+		int a = 2141011173;
+		int b = 2021;
+		System.out.println("My Regd.No is "+a+" and I have taken admission in B.Tech.In " + b);
+
+	}
+
+}
