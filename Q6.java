0a5212fad0abd69058c794eb6538633912d321ba
ritukumari2004 <127641811+ritukumari2004@users.noreply.github.com>
2023-03-19 22:28:49 +0530

    Add files via upload
    Q6

---
+++ Q6.java
@@ -0,0 +1,13 @@
+ 
+
+public class Q6 {
+
+	public static void main(String[] args) {
+		int a = 113;
+	    double b = 2.71828;
+	    System.out.println("This is room # " +a);
+	    System.out.println("e is close to " +b);
+
+	}
+
+}
