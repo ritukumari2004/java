f775f5f7a08f91f05471460b6082ff9e8ddc6bfe
ritukumari2004 <127641811+ritukumari2004@users.noreply.github.com>
2023-03-22 23:42:41 +0530

    Add files via upload
    Q6

---
+++ Q7.java
@@ -0,0 +1,18 @@
+
+public class Q7 {
+
+	public static void main(String[] args) {
+		int A = 2;
+		int B = 3;
+		System.out.println("The value of A is "+A);
+		System.out.println("The value of B is "+B);
+		int C;
+		C=A;
+		A=B;
+		B=C;
+		System.out.println("now, The value of A is "+A);
+		System.out.println("now, The value of B is "+B);
+
+	}
+
+}
