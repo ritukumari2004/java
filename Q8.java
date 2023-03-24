201ba231c8c2b7657127f64983c9c9db6bf356c6
ritukumari2004 <127641811+ritukumari2004@users.noreply.github.com>
2023-03-24 23:44:22 +0530

    Add files via upload
    Q8

---
+++ Q8.java
@@ -0,0 +1,16 @@
+
+
+public class Q8 {
+
+	public static void main(String[] args) {
+		int A = 2;
+		int B = 3;
+		A = A+B;
+		B = A-B;
+		A = A-B;
+		System.out.println("The value of A is "+A);
+		System.out.println("The value of B is "+B);
+
+	}
+
+}
